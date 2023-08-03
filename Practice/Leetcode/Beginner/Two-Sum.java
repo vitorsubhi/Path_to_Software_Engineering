55ea2f37bac5b70d6e02ac395550a99b6a2d65c4
Vitor Subhi <vitorsubhi@gmail.com>
2023-08-03 20:03:17 -0300

    https://leetcode.com/problems/two-sum

--- Practice/Leetcode/Beginner/Two-Sum.java
+++ Practice/Leetcode/Beginner/Two-Sum.java
@@ -1,20 +1,20 @@
+//Brute Force
+//Time Complexity: O(n²)
+//Space Complexity: O(1)
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         int i = 0;
         int j = 0;
-        int retorno[] = {0, 0};
         for (i = 0; i < nums.length; i++)
         {
             for (j = nums.length-1; j > i; j--)
             {
                 if(nums[i] + nums[j] == target)
                 {
-                    retorno[0] = i;
-                    retorno[1] = j;
-                    break;
+                    return new int[] { i, j };
                 }
             }
         }
-        return retorno;
+        return null;
     }
 }
