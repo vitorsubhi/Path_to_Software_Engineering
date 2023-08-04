b74a8350c45bce03164789b0251480216dc2c5dc
Vitor Almeida Subhi <vitorsubhi@gmail.com>
2023-08-04 14:26:23 -0300

    https://leetcode.com/problems/palindrome-number/

---
+++ Practice/Leetcode/Beginner/Java/Palindrome.java
@@ -0,0 +1,16 @@
+//Time Complexity: O(n)
+//Space Complexity: O(1)
+class Solution {
+    public boolean isPalindrome(int x) {
+        String y = Integer.toString(x);
+        int i, j;
+        for (i = 0, j = y.length()-1; i < j; i++, j--)
+        {
+            if (y.charAt(i) != y.charAt(j))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
