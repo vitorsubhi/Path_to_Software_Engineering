a8eb364926f66fe5a2a369592a2b824d3b535b71
Vitor Almeida Subhi <vitorsubhi@gmail.com>
2023-08-04 13:42:55 -0300

    https://www.beecrowd.com.br/judge/pt/problems/view/1004

---
+++ Practice/Beecrowd/Iniciante/C99/1004 - Produto Simples.c
@@ -0,0 +1,12 @@
+//Time Complexity: O(1)
+//Space Complexity: O(1)
+#include <stdio.h>
+
+int main(){
+    int A, B, PROD;
+    scanf ("%d", &A);
+    scanf ("%d", &B);
+    PROD = A * B;
+    printf ("PROD = %d\n", PROD);
+    return 0;
+}
