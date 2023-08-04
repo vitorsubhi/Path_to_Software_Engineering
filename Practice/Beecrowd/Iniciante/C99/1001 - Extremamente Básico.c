c76254c7f3d7fa6d76c3a583ee9ec51df0436d5e
Vitor Almeida Subhi <vitorsubhi@gmail.com>
2023-08-04 11:46:08 -0300

    https://www.beecrowd.com.br/judge/pt/problems/view/1001

---
+++ Practice/Beecrowd/Iniciante/C99/1001 - Extremamente Básico.c
@@ -0,0 +1,12 @@
+//Time Complexity: O(1)
+//Space Complexity: O(1)
+#include <stdio.h>
+
+int main(){
+    int A, B, X;
+    scanf ("%d", &A);
+    scanf ("%d", &B);
+    X = A + B;
+    printf ("X = %d\n", X);
+    return 0;
+}
