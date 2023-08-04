a5a2dab2c5a0b94b85ccb37aad1e9ac34d0b8470
Vitor Almeida Subhi <vitorsubhi@gmail.com>
2023-08-04 13:51:39 -0300

    https://www.beecrowd.com.br/judge/pt/problems/view/1005

---
+++ Practice/Beecrowd/Iniciante/C99/1005 - Média 1.c
@@ -0,0 +1,12 @@
+//Time Complexity: O(1)
+//Space Complexity: O(1)
+#include <stdio.h>
+
+int main(){
+    double A, B, MEDIA;
+    scanf ("%lf", &A);
+    scanf ("%lf", &B);
+    MEDIA = (((A * 0.35) + (B * 0.75)) * 10.0) / 11.00;
+    printf ("MEDIA = %.5f\n", MEDIA);
+    return 0;
+}
