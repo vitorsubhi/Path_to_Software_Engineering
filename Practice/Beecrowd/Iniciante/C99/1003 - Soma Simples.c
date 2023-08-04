5afc6effa0cd96be232e7fa15ddd7e8dd42886f9
Vitor Almeida Subhi <vitorsubhi@gmail.com>
2023-08-04 13:41:16 -0300

    https://www.beecrowd.com.br/judge/pt/problems/view/1003

---
+++ Practice/Beecrowd/Iniciante/C99/1003 - Soma Simples.c
@@ -0,0 +1,12 @@
+//Time Complexity: O(1)
+//Space Complexity: O(1)
+#include <stdio.h>
+
+int main(){
+    int A, B, SOMA;
+    scanf ("%d", &A);
+    scanf ("%d", &B);
+    SOMA = A + B;
+    printf ("SOMA = %d\n", SOMA);
+    return 0;
+}
