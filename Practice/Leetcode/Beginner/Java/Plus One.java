cfb1ed06cc0dc0da7075a2d63135513598e0a3de
Vitor Subhi <vitorsubhi@gmail.com>
2023-08-07 20:59:07 -0300

    https://leetcode.com/problems/plus-one/

---
+++ Practice/Leetcode/Beginner/Java/Plus One.java
@@ -0,0 +1,17 @@
+
+//Time Complexity: O(n)
+//Space Complexity: O(1)
+class Solution {
+    public int[] plusOne(int[] digits) {
+        for (int i=digits.length-1; i >= 0; i--){
+            if (digits[i] < 9){
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        digits = new int[digits.length + 1];
+        digits[0] = 1;
+        return digits;
+    }
+}
