94b2752db5391d1404c8953f2adfc4cd0ee46a96
Vítor Almeida Subhi <vitorsubhi@gmail.com>
2025-02-01 13:42:41 +0000

    feat(leetcode): add solution to problem 206 - reverse linked list

---
+++ Practice/Leetcode/Beginner/Java/206 - Reverse Linked List.java
@@ -0,0 +1,16 @@
+//Time Complexity: O(n)
+//Space Complexity: O(1)
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+
+        return prev;
+    }
+}
